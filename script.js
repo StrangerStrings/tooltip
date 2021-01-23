ec730fd718ec487670e509a5d907afd6d2624cde
Ben Green <Ben.Green@crispthinking.com>
2021-01-23 18:04:52 +0000

    perfecto

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const thmb = document.querySelector('.thumbnail')
 const tStyle = getComputedStyle(thmb);
-const tSize = parseInt(tStyle.inlineSize)
+const tWidth = parseInt(tStyle.inlineSize)
 const tMarg = parseInt(tStyle.marginLeft)
-const tTotal = tSize + 2*tMarg
+const tTotal = tWidth + 2*tMarg
 
 let debounce = false
 
@@ -14,16 +14,16 @@ const onResize = new ResizeObserver(entries => {
     setTimeout(()=> {
         const cntr = document.querySelector('.container')
         const cStyle = getComputedStyle(cntr);
-        const cSize = parseInt(cStyle.inlineSize) 
+        const cWidth = parseInt(cStyle.inlineSize) 
         const cPadd = parseInt(cStyle.paddingLeft)
-        const cTotal = cSize - 2*cPadd
+        const cTotal = cWidth - 2*cPadd
          
         const itemsPerRow = Math.floor(cTotal / tTotal);
     
         const thumbnails = document.querySelectorAll('.thumbnail')
         for (let i = 0; i < thumbnails.length; i++) {
             const thumb = thumbnails[i]
-            const lastOnRow = (i+1)%itemsPerRow == 0
+            const lastOnRow = (i+1) % itemsPerRow == 0
             if (lastOnRow) {
                 thumb.classList.add('edge')
             } else {
@@ -40,7 +40,6 @@ onResize.observe(container)
 //add content 
 const tooltips = document.querySelectorAll('.info')
 for (let i = 0; i < tooltips.length; i++) {
-    const tip = tooltips[i];
     const rand = Math.ceil(Math.random()*15.8)
-    tip.innerHTML = '<p>qwertyuiop:123456789</p>'.repeat(rand)
+    tooltips[i].innerHTML = '<p>qwertyuiop:123456789</p>'.repeat(rand)
 }
